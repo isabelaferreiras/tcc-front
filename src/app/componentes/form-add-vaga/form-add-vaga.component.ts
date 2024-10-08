6b7de2f8ef028f2b88f8a17528924f5ecaa180b9
Isabela Ferreira <isabela.ferreiira16@gmail.com>
2024-10-08 02:40:31 -0300

    commit

--- src/app/componentes/form-add-vaga/form-add-vaga.component.ts
+++ src/app/componentes/form-add-vaga/form-add-vaga.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { VagaService } from '../../services/vaga.service';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-add-vaga',
   standalone: true,
   imports: [],
   templateUrl: './form-add-vaga.component.html',
-  styleUrl: './form-add-vaga.component.css'
+  styleUrl: './form-add-vaga.component.css',
+  providers: [VagaService]
 })
 export class FormAddVagaComponent {
 
+  constructor(private vagaService: VagaService, private router: Router) {}
+
+  form = new FormGroup({
+    titulo: new FormControl('', [Validators.required]),
+    empresaId: new FormControl(Number(localStorage.getItem('id'))),
+    endereco: new FormGroup({
+      cidade: new FormControl('', [Validators.required]),
+      estado: new FormControl('', [Validators.required])
+    }),
+    descricao: new FormControl('', [Validators.required]),
+    cargo: new FormControl('', [Validators.required])
+  })
 }
